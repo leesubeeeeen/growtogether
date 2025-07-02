eeff96f29fdc8c3bd79d98bc0091d802d708548b
leesubeeeeen <ahdtlfdl5356@naver.com>
2025-07-02 20:08:06 +0900

    feat: 캘린더 UI구현

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'screens/start_screen.dart';
 import 'screens/schedule_preference_page.dart';
 import 'screens/add_schedule_bottom_screen.dart';
+import 'screens/calendar_screen.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('ko_KR', null); // <-- 로케일 초기화
+  runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -14,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SchedulePreferencePage(),
+      home: CalendarScreen(),
     );
   }
 }
