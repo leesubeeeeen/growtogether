c804243c9f9c486f04045a13cc0e7aa8d8134d92
leesubeeeeen <ahdtlfdl5356@naver.com>
2025-07-08 17:06:21 +0900

    feat: 상담사 페르소나 선택 화면 구현

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/calendar_screen.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'screens/home_page.dart';
+import 'screens/counselor_selection_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,9 +16,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: CounselorSelectionPage(),
     );
   }
 }
 //
 //StartScreen()
+//HomePage()
