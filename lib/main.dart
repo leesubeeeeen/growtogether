1ab6a01c83fe5451264f9304addf97ff4b6a9d7f
leesubeeeeen <ahdtlfdl5356@naver.com>
2025-07-21 13:21:33 +0900

    fix: ai-chat화면 main.dart 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:growtogether/pages/gpt_test_page.dart';
 import 'package:provider/provider.dart';
 import 'providers/user_provider.dart';
 import 'providers/partner_provider.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:growtogether/screens//counselor_chat_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: GPTTestPage(),
+      home: CounselorChatPage(),
     );
   }
 
